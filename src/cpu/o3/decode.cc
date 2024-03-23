060497c7620b4e7f11a9844a673c2925f081a6ea
luot-ai <1035768203@qq.com>
2024-03-23 09:57:13 +0800

    issue：special for custom ins as they have not "src

--- src/cpu/o3/decode.cc
+++ src/cpu/o3/decode.cc
@@ -672,7 +672,7 @@ Decode::decodeInsts(ThreadID tid)
         // them as ready to issue at any time.  Not sure if this check
         // should exist here or at a later stage; however it doesn't matter
         // too much for function correctness.
-        if (inst->numSrcRegs() == 0) {
+        if (inst->numSrcRegs() == 0  && !inst->isCustom() ) {
             inst->setCanIssue();
         }
 
